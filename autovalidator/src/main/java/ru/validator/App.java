3da104813c85c7966cf6d268c5b3b8dcd42054be
mstefani-source <ivanow.victor@live.ru>
2025-01-29 00:18:44 +0300

    first commit

---
+++ autovalidator/src/main/java/ru/validator/App.java
@@ -0,0 +1,54 @@
+package ru.validator;
+
+import java.util.Set;
+
+import com.networknt.schema.InputFormat;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SchemaLocation;
+import com.networknt.schema.SchemaValidatorsConfig;
+import com.networknt.schema.SpecVersion.VersionFlag;
+import com.networknt.schema.ValidationMessage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(VersionFlag.V202012, builder ->
+        // This creates a mapping from $id which starts with https://www.example.org/ to
+        // the retrieval URI classpath:schema/
+        builder.schemaMappers(
+                schemaMappers -> schemaMappers.mapPrefix("https://www.example.org/", "classpath:schema/")));
+        SchemaValidatorsConfig.Builder builder = SchemaValidatorsConfig.builder();
+        // By default the JDK regular expression implementation which is not ECMA 262
+        // compliant is used
+        // Note that setting this requires including optional dependencies
+        // builder.regularExpressionFactory(GraalJSRegularExpressionFactory.getInstance());
+        // builder.regularExpressionFactory(JoniRegularExpressionFactory.getInstance());
+        SchemaValidatorsConfig config = builder.build();
+
+        // Due to the mapping the schema will be retrieved from the classpath at
+        // classpath:schema/example-main.json.
+        // If the schema data does not specify an $id the absolute IRI of the schema
+        // location will be used as the $id.
+        JsonSchema schema = jsonSchemaFactory.getSchema(SchemaLocation.of("https://www.example.org/example-main.json"),
+                config);
+        String input = "{\r\n"
+                + "  \"main\": {\r\n"
+                + "    \"common\": {\r\n"
+                + "      \"field\": \"invalidfield\"\r\n"
+                + "    }\r\n"
+                + "  }\r\n"
+                + "}";
+
+        Set<ValidationMessage> assertions = schema.validate(input, InputFormat.JSON, executionContext -> {
+            // By default since Draft 2019-09 the format keyword only generates annotations
+            // and not assertions
+            executionContext.getExecutionConfig().setFormatAssertionsEnabled(true);
+        });
+
+    }
+}
